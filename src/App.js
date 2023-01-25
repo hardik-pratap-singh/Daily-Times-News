ca62554904e53a32d3395dab030ecff079be56ac
hardik-pratap-singh <hardikjanuary2021@gmail.com>
2023-01-25 18:45:58 +0530

    V 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Navbar from "../src/components/Navbar"
+import { useEffect , useState } from "react"
+import axios from "axios" 
+import './App.css'
+import Newsitem from "./components/Newsitem";
+
+
 
 function App() {
+
+
+  // let a = [] ; 
+  const [text, setText] = useState('India'); 
+  const [news , setNews ] = useState([]) ; 
+  useEffect(() => {
+    axios.get("https://newsapi.org/v2/everything?q="+text+"&sources=bbc-news&apiKey=ef321c68074544eeb8b728d3d1c16d3a&lang=en")
+    // axios.get("https://newsapi.org/v2/everything?sources=bbc-news&apiKey=ef321c68074544eeb8b728d3d1c16d3a&lang=en")
+  .then((x) => {
+    setNews(x.data.articles) ; 
+  })
+
+
+  },[text, setText]); 
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar text = {text} settext = {setText}  />
+    <br /><br />
+    <h3 className = "mx-3" style={{fontWeight : 800}}> {`Displaying News Related to - ${text}`}</h3>
+    <br /><br />
+    
+    <div className="row">
+    { 
+      news.map((x) =>{
+        return (
+
+          <Newsitem urlToImage={x.urlToImage} title = {x.title} desc = {x.description} url={x.url} key={x.publishedAt}/>
+        
+        )
+      } )
+    }
     </div>
+
+    </>
   );
 }
 
